c409aaf487661550857e8418ddde56f342077aa1
Petter Rasmussen <petter@hask.no>
2022-08-24 10:39:09 +0300

    Add build command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use crate::build::Runner;
 mod build;
 mod exec;
 mod project;
@@ -8,7 +9,9 @@ mod typescript_builder;
 mod watch;
 
 use crate::project::Project;
+use crate::rust_builder::RustBuilder;
 use crate::script_runner::ScriptRunner;
+use crate::typescript_builder::TypeScriptBuilder;
 use build::Env;
 use clap::{Parser, Subcommand};
 use project_info::ProjectInfo;
@@ -31,11 +34,24 @@ enum Commands {
         name: String,
     },
 
+    /// Build the project
+    #[clap(arg_required_else_help = false)]
+    Build {
+        /// Release build
+        #[clap(long)]
+        release: bool,
+
+        /// Post build script to run after build
+        #[clap(long)]
+        script: Option<String>,
+    },
+
     /// Watch for changes and build
     #[clap(arg_required_else_help = true)]
     Watch {
         /// Post build script to run after build
-        script: String,
+        #[clap(long)]
+        script: Option<String>,
     },
 }
 
@@ -55,13 +71,48 @@ fn main() {
             println!("{:?}", res);
         }
 
-        Commands::Watch { script } => {
+        Commands::Build { script, release } => {
+            let env = if release { Env::Release } else { Env::Dev };
             let current_dir = get_current_dir();
-            let script_path = current_dir.join(script);
             let project_info = ProjectInfo::from_dir(&current_dir).unwrap();
-            let env = Env::Dev;
 
             print_project_info(&project_info);
+
+            let rust_builder =
+                RustBuilder::new(rust_builder::Config::from_project_info(&env, &project_info));
+
+            let typescript_builder = TypeScriptBuilder::new(
+                typescript_builder::Config::from_project_info(&env, &project_info),
+            );
+
+            rust_builder.run().expect("Rust build failed");
+            typescript_builder.run().expect("TypeScript build failed");
+
+            if let Some(script_name) = script {
+                let script_path = current_dir.join(script_name);
+                let script_runner = ScriptRunner::new(script_path);
+                script_runner.run().expect("Post build runner failed");
+            }
+        }
+
+        Commands::Watch { script } => {
+            let env = Env::Dev;
+            let current_dir = get_current_dir();
+            let project_info = ProjectInfo::from_dir(&current_dir).unwrap();
+
+            print_project_info(&project_info);
+
+            let post_build_runner = if let Some(script_name) = script {
+                let script_path = current_dir.join(script_name);
+                if script_path.exists() {
+                    Some(ScriptRunner::new(script_path))
+                } else {
+                    eprintln!("Could not find script: {}", script_path.display());
+                    None
+                }
+            } else {
+                None
+            };
 
             let builder = build::Builder::new(build::Config {
                 rust_builder: rust_builder::RustBuilder::new(
@@ -70,9 +121,7 @@ fn main() {
                 typescript_builder: typescript_builder::TypeScriptBuilder::new(
                     typescript_builder::Config::from_project_info(&env, &project_info),
                 ),
-                post_build_runner: script_path
-                    .exists()
-                    .then_some(ScriptRunner::new(script_path)),
+                post_build_runner,
             });
 
             let watcher_config = watch::Config::new(&current_dir, builder);
